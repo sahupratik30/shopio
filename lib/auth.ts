3a5fa4693722ca288b5915c8e5c96cddc0f99e26
sahupratik30 <sahupratik30@gmail.com>
2023-09-09 12:19:11 +0530

    basic auth setup done

---
+++ lib/auth.ts
@@ -0,0 +1,11 @@
+import type { NextAuthOptions as NextAuthConfig } from "next-auth";
+import Google from "next-auth/providers/google";
+
+export const authConfig = {
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+} satisfies NextAuthConfig;
