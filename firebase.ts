c0f4a7dac5975d7816c6b6cfb62915cc1032602e
sahupratik30 <sahupratik30@gmail.com>
2023-10-29 10:47:08 +0530

    added firebase config

---
+++ firebase.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBTGFDfq9GSxsGd9tjs3xOtF7b4gxkMQmM",
+  authDomain: "shopio-13722.firebaseapp.com",
+  projectId: "shopio-13722",
+  storageBucket: "shopio-13722.appspot.com",
+  messagingSenderId: "183849052086",
+  appId: "1:183849052086:web:c34cde21f2f2767e268317",
+};
