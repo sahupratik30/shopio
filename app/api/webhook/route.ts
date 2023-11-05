4e70078e5ed5ec4185b2c9df2710ad834d105bf0
sahupratik30 <sahupratik30@gmail.com>
2023-11-06 00:18:23 +0530

    added webhook that verifies payment and saves order to database

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,70 @@
+import { headers } from "next/headers";
+import * as admin from "firebase-admin";
+import serviceAccount from "../../../permissions.json";
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+// initialize firebase app
+const app = !admin.apps.length
+  ? admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    })
+  : admin.app();
+
+// save order to database
+const saveOrder = async (session: Stripe.Checkout.Session) => {
+  try {
+    await app
+      .firestore()
+      .collection("users")
+      .doc((session.metadata as Stripe.Metadata).email)
+      .collection("orders")
+      .doc(session.id)
+      .set({
+        amount: (session.amount_total as number) / 100,
+        timestamp: admin.firestore.FieldValue.serverTimestamp(),
+        products: JSON.parse((session.metadata as Stripe.Metadata).products),
+      });
+  } catch (error) {
+    console.log("Error occured while saving order:", error);
+  }
+};
+
+export async function POST(req: Request) {
+  const payload = await req.text();
+  const headersList = headers();
+  const signature = headersList.get("stripe-signature");
+
+  let event: Stripe.Event | null = null;
+  try {
+    event = stripe.webhooks.constructEvent(payload, signature, webhookSecret);
+    switch (event?.type) {
+      // listen to 'checkout.session.completed' event
+      case "checkout.session.completed":
+        const session = event.data.object;
+        await saveOrder(session);
+        return NextResponse.json({ error: false }, { status: 200 });
+      default:
+        // other events that we don't handle
+        return NextResponse.json({ error: false }, { status: 200 });
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err.message);
+      return NextResponse.json(
+        { error: true, message: err.message },
+        { status: 400 }
+      );
+    }
+  }
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+    externalResolver: true,
+  },
+};
