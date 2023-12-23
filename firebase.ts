ba5cb70cf1fad46ce10bc4e3e7ea033909cd2686
Pratik Sahu <pratiks@logic-square.com>
2023-12-23 21:25:44 +0530

    updated firebase config

--- firebase.ts
+++ firebase.ts
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBTGFDfq9GSxsGd9tjs3xOtF7b4gxkMQmM",
-  authDomain: "shopio-13722.firebaseapp.com",
-  projectId: "shopio-13722",
-  storageBucket: "shopio-13722.appspot.com",
-  messagingSenderId: "183849052086",
-  appId: "1:183849052086:web:c34cde21f2f2767e268317",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
